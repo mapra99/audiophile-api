a15fddb1d5799b41bccd70462d269b38e768f125
Miguel Prada <mapra99@gmail.com>
2022-06-05 15:36:22 -0500

    feat(Products): Implement #show endpoint on admin

--- spec/controllers/admin/v1/products_controller_spec.rb
+++ spec/controllers/admin/v1/products_controller_spec.rb
@@ -121,4 +121,48 @@ RSpec.describe Admin::V1::ProductsController, type: :controller do
       end
     end
   end
+
+  describe '#show' do
+    let(:product) { create(:product) }
+    let(:finder_result) do
+      instance_double('Finder Result', success?: true, failure?: false, value!: product)
+    end
+    let(:finder) { instance_double(Admin::V1::Products::Finder, call: finder_result) }
+
+    before do
+      allow(Admin::V1::Products::Finder).to receive(:new).and_return finder
+
+      request.headers['X-ADMIN-KEY'] = 'admin'
+      get :show, format: :json, params: { id: product.id }
+    end
+
+    it 'calls the collection builder service' do
+      expect(finder).to have_received(:call)
+    end
+
+    it 'returns a 200 status' do
+      expect(response.status).to eq 200
+    end
+
+    describe 'when finder fails' do
+      let(:finder_result) do
+        instance_double('Finder Result', success?: false, failure?: true, failure: { code: :not_found })
+      end
+
+      it 'returns a 404 status' do
+        expect(response.status).to eq 404
+      end
+    end
+
+    describe 'when not authenticated' do
+      before do
+        request.headers['X-ADMIN-KEY'] = nil
+        get :show, format: :json, params: { id: product.id }
+      end
+
+      it 'returns a 401 error' do
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end
