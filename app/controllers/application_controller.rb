156bc10cfc66de09c05bb3ae0b50de711e2f988b
Miguel Prada <mapra99@gmail.com>
2022-06-06 21:53:21 -0500

    feat(ProductContents): implement #create endpoint for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::API
   GLOBAL_ERROR_CODES = {
     internal_error: 500,
-    not_found: 404
+    not_found: 404,
+    wrong_params: 400
   }.freeze
 
   GLOBAL_ERROR_MESSAGES = {
     internal_error: 'Internal Server Error',
-    not_found: 'Resource Not Found'
+    not_found: 'Resource Not Found',
+    wrong_params: 'Bad request'
   }.freeze
 
   private
@@ -27,7 +29,7 @@ class ApplicationController < ActionController::API
   end
 
   def error_message(error)
-    message = GLOBAL_ERROR_MESSAGES[error[:code]]
+    message = error[:message] || GLOBAL_ERROR_MESSAGES[error[:code]]
     raise StandardError "Error message could not be processed for error #{error}" if message.blank?
 
     message
