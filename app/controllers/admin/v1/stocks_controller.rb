e41a2f4d4d1facdcb77ec1dad10457a719db8f15
Miguel Prada <mapra99@gmail.com>
2022-07-04 22:37:46 -0500

    feat(ProductStocks): build #create admin endpoint

---
+++ app/controllers/admin/v1/stocks_controller.rb
@@ -0,0 +1,49 @@
+module Admin
+  module V1
+    class StocksController < BaseController
+      PRODUCT_ERROR_CODES = {
+        product_not_found: 400,
+        stock_not_saved: 500,
+        invalid_stock: 422,
+        invalid_toppings: 422
+      }.freeze
+
+      PRODUCT_ERROR_MESSAGES = {
+        product_not_found: 'Could not find product',
+        stock_not_saved: 'Could not save stock',
+        invalid_stock: 'Stock is invalid: ',
+        invalid_toppings: 'Toppings are invalid: '
+      }.freeze
+
+      def create
+        creator = Admin::V1::Stocks::Creator.new(params: stock_params)
+        result = creator.call
+        return render_error_from(result) if result.failure?
+
+        @stock = result.value!
+      end
+
+      private
+
+      def stock_params
+        params.permit(:product_id, :quantity, toppings: %i[key value price_change])
+      end
+
+      def error_status_code(error)
+        product_error_code = PRODUCT_ERROR_CODES[error[:code]]
+        return product_error_code if product_error_code.present?
+
+        super(error)
+      end
+
+      def error_message(error)
+        product_error_message = PRODUCT_ERROR_MESSAGES[error[:code]]
+        product_error_message += error[:message].join(', ') if error[:code] == :invalid_stock
+        product_error_message += error[:message] if error[:code] == :invalid_toppings
+        return product_error_message if product_error_message.present?
+
+        super(error)
+      end
+    end
+  end
+end
---
+++ app/services/admin/v1/stocks/creator.rb
@@ -0,0 +1,78 @@
+module Admin
+  module V1
+    module Stocks
+      class Creator
+        include Dry::Monads[:result]
+
+        attr_reader :params, :stock
+
+        def initialize(params:)
+          self.params = params
+        end
+
+        def call
+          ActiveRecord::Base.transaction do
+            find_product
+            build_stock
+            save_stock
+            save_toppings
+          end
+
+          Success(stock)
+        rescue ServiceError => e
+          e.failure
+        rescue StandardError => e
+          Rails.logger.error(e)
+          Failure({ code: :internal_error })
+        end
+
+        private
+
+        attr_writer :params, :stock
+        attr_accessor :product
+
+        def find_product
+          self.product = Product.find(params[:product_id])
+        rescue ActiveRecord::RecordNotFound => e
+          Rails.logger.error(e)
+          raise ServiceError, Failure({ code: :product_not_found })
+        end
+
+        def build_stock
+          self.stock = Stock.new(
+            product: product,
+            quantity: params[:quantity]
+          )
+        end
+
+        def save_stock
+          stock.save!
+        rescue ActiveRecord::RecordNotSaved => e
+          Rails.logger.error(e)
+          raise ServiceError, Failure({ code: :stock_not_saved })
+        rescue ActiveRecord::RecordInvalid => e
+          Rails.logger.error(e)
+          raise ServiceError, Failure({ code: :invalid_stock, message: stock.errors.full_messages })
+        end
+
+        def save_toppings
+          params[:toppings].each { |topping_params| save_topping(topping_params) }
+        end
+
+        def save_topping(topping_params)
+          topping = product.toppings.find_or_initialize_by(
+            key: topping_params[:key],
+            value: topping_params[:value]
+          )
+
+          topping.price_change = topping_params[:price_change]
+          topping.save!
+          stock.toppings << topping
+        rescue ActiveRecord::RecordInvalid => e
+          Rails.logger.error(e)
+          raise ServiceError, Failure({ code: :invalid_toppings, message: e.message })
+        end
+      end
+    end
+  end
+end
