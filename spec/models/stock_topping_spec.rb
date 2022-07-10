5f36787e355577043f1cd07281b45faadbad0218
Miguel Prada <mapra99@gmail.com>
2022-07-09 22:07:16 -0500

    Add specs

--- spec/models/stock_topping_spec.rb
+++ spec/models/stock_topping_spec.rb
@@ -7,4 +7,38 @@ RSpec.describe StockTopping, type: :model do
     it { is_expected.to belong_to(:stock) }
     it { is_expected.to belong_to(:topping) }
   end
+
+  describe 'validations' do
+    describe 'uniqueness of topping keys within scope of stock' do
+      subject(:stock_topping) { build(:stock_topping, stock: stock, topping: topping) }
+
+      let(:stock) { create(:stock) }
+      let(:topping) { create(:topping, key: 'something') }
+
+      before do
+        create(:topping, key: 'something', stock_toppings: [stock_topping])
+      end
+
+      it 'is not valid' do
+        expect(stock_topping).not_to be_valid
+      end
+    end
+
+    describe 'uniqueness of stock toppings key-value pairs within scope of product' do
+      subject(:stock_topping) { build(:stock_topping, stock: stock1, topping: topping) }
+
+      let(:product) { create(:product) }
+      let(:stock1) { create(:stock, product: product) }
+      let(:stock2) { create(:stock, product: product) }
+      let(:topping) { create(:topping, product: product, key: 'color', value: 'blue', stocks: [stock2]) }
+
+      before do
+        topping.save
+      end
+
+      it 'is not valid' do
+        expect(stock_topping).not_to be_valid
+      end
+    end
+  end
 end
