474f76f7e825b72782a45ac10ee9623ed16d9f52
Miguel Prada <mapra99@gmail.com>
2022-08-28 16:11:03 -0500

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # draw(:sidekiq)
   draw(:sidekiq)
   draw(:admin)
   draw(:api)
