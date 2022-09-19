f2190536cc728341c6b0f1c4a4dc26e5f05b7a09
Miguel Prada <mapra99@gmail.com>
2022-09-18 22:33:50 -0500

    Comment out vendor services that block rails startup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-Sidekiq.configure_server do |config|
-  config.redis = {
-    url: Rails.env.production? ? ENV.fetch('REDISCLOUD_URL') : ENV.fetch('REDIS_URL', 'redis://localhost:6379')
-  }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = {
+#     url: Rails.env.production? ? ENV.fetch('REDISCLOUD_URL') : ENV.fetch('REDIS_URL', 'redis://localhost:6379')
+#   }
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = {
-    url: Rails.env.production? ? ENV.fetch('REDISCLOUD_URL') : ENV.fetch('REDIS_URL', 'redis://localhost:6379')
-  }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = {
+#     url: Rails.env.production? ? ENV.fetch('REDISCLOUD_URL') : ENV.fetch('REDIS_URL', 'redis://localhost:6379')
+#   }
+# end
