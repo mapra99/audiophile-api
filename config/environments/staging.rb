4dee5b8e04b10e41f42c3228685deb3b2ad03d02
Miguel Prada <mapra99@gmail.com>
2022-10-01 17:12:25 -0500

    allow all hosts on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.hosts << /.+\.ngrok\.io\z/
+  config.hosts << /.+/
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
