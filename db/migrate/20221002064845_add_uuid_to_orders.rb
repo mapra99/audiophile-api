4f5b61c39335bd20a34925629cd3347d58eff95a
Miguel Prada <mapra99@gmail.com>
2022-10-02 02:16:25 -0500

    Linter fix

--- db/migrate/20221002064845_add_uuid_to_orders.rb
+++ db/migrate/20221002064845_add_uuid_to_orders.rb
@@ -1,6 +1,6 @@
 class AddUuidToOrders < ActiveRecord::Migration[6.1]
   def change
     add_column :orders, :uuid, :string, null: false
-    add_index :orders, :uuid, :unique => true
+    add_index :orders, :uuid, unique: true
   end
 end
