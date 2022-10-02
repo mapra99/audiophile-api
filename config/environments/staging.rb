40084059cbec77918f1d5f965d636720dcf72e71
Miguel Prada <mapra99@gmail.com>
2022-10-02 00:57:15 -0500

    use amazon storage on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = ENV.fetch('DEV_ACTIVE_STORAGE_PROVIDER', 'local').to_sym
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
