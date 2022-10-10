9d96d35bc4a97c74b52211ea459f2befb55ce4d5
Miguel Prada <mapra99@gmail.com>
2022-10-10 12:13:50 -0500

    feat(PurchaseCarts): add #update endpoint to PATCH user_location (#139)
    * feat(PurchaseCarts): add #update endpoint to PATCH user_location
    
    * Linter fixes

---
+++ app/actions/api/v1/purchase_carts/updater.rb
@@ -0,0 +1,46 @@
+module Api
+  module V1
+    module PurchaseCarts
+      class Updater
+        include Dry::Monads[:result]
+
+        attr_reader :params, :purchase_cart
+
+        def initialize(params:, session:)
+          self.params = params
+          self.session = session
+        end
+
+        def call
+          ActiveRecord::Base.transaction do
+            find_cart
+            update_user_location
+          end
+
+          Success(purchase_cart)
+        rescue ServiceError => e
+          e.failure
+        rescue StandardError => e
+          Rails.logger.error(e)
+          Failure({ code: :internal_error })
+        end
+
+        private
+
+        attr_writer :params, :purchase_cart
+        attr_accessor :session
+
+        def find_cart
+          self.purchase_cart = session.purchase_carts.find_by!(uuid: params[:uuid])
+        rescue ActiveRecord::RecordNotFound => e
+          raise ServiceError, Failure({ code: :cart_not_found, message: e.message })
+        end
+
+        def update_user_location
+          user_location = UserLocation.find_by!(uuid: params[:user_location_uuid])
+          purchase_cart.update!(user_location: user_location)
+        end
+      end
+    end
+  end
+end
---
+++ spec/actions/api/v1/purchase_carts/updater_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::PurchaseCarts::Updater do
+  subject(:updater) { described_class.new(params: params, session: session) }
+
+  let(:session) { create(:session) }
+  let(:purchase_cart) { create(:purchase_cart, session: session, user_location: nil) }
+  let(:user_location) do
+    VCR.use_cassette('geocoder_response', match_requests_on: %i[method host path]) do
+      create(:user_location)
+    end
+  end
+  let(:user_location_uuid) { user_location.uuid }
+  let(:purchase_cart_uuid) { purchase_cart.uuid }
+  let(:params) do
+    {
+      uuid: purchase_cart_uuid,
+      user_location_uuid: user_location_uuid
+    }
+  end
+
+  describe '#call' do
+    subject(:result) { updater.call }
+
+    it 'succeeds' do
+      expect(result.success?).to eq(true)
+    end
+
+    it 'updates the cart user_location' do
+      updater.call
+
+      expect(purchase_cart.reload.user_location).to eq(user_location)
+    end
+
+    describe 'when service failes due to unhandled error' do
+      let(:user_location_uuid) { 'abc123' }
+
+      it 'returns a failure' do
+        expect(result.failure?).to eq(true)
+      end
+
+      it 'returns internal_error as failure code' do
+        expect(result.failure[:code]).to eq(:internal_error)
+      end
+    end
+
+    describe "when cart isn't found" do
+      let(:purchase_cart_uuid) { 'abc123' }
+
+      it 'returns a failure' do
+        expect(result.failure?).to eq(true)
+      end
+
+      it 'returns the right error code' do
+        expect(result.failure[:code]).to eq(:cart_not_found)
+      end
+    end
+  end
+end
