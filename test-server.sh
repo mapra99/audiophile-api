d64b5d73f2f3c140f881049b0f2a0c2f267be8e2
Miguel Prada <mapra99@gmail.com>
2022-11-25 13:10:13 -0500

    run in background

--- test-server.sh
+++ test-server.sh
@@ -2,4 +2,4 @@
 RAILS_ENV=test bundle exec rails db:reset &&
 RAILS_ENV=test bundle exec rails db:schema:load &&
 RAILS_ENV=test bundle exec rake seeds:e2e &&
-RAILS_ENV=test bundle exec rails server -p 3001
+RAILS_ENV=test bundle exec rails server -p 3001 &
