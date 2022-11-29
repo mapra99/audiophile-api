4be5ba3fb7604c43c8f092d1eb3c26cc89da09a8
Miguel Prada <mapra99@gmail.com>
2022-11-29 04:45:42 -0500

    feat(Products): filter #index by product contents

--- app/actions/api/v1/products/collection_builder.rb
+++ app/actions/api/v1/products/collection_builder.rb
@@ -8,6 +8,7 @@ module Api
           self.result = Product.all
           filter_by_category(filters[:category_ids]) if filters[:category_ids].present?
           filter_by_featured(filters[:featured]) if filters[:featured].present?
+          filter_by_content(filters[:content]) if filters[:content].present?
 
           Success(result)
         rescue StandardError => e
@@ -27,6 +28,16 @@ module Api
 
         def filter_by_featured(featured)
           self.result = result.where(featured: featured)
+        end
+
+        def filter_by_content(content_filters)
+          product_contents = ProductContent.all
+
+          content_filters.to_h.each do |key, value|
+            product_contents = product_contents.where('key = ? AND value = ?', key, value)
+          end
+
+          self.result = result.where(id: product_contents.pluck(:product_id))
         end
       end
     end
