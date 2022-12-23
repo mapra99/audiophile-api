a8a22b1b147db1169cc77700313ae912398bab0c
Miguel Prada <mapra99@gmail.com>
2022-12-23 14:39:35 -0500

    feat(products): build update endpoint

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -3,7 +3,7 @@ namespace :admin do
     resource :health, only: [:show], controller: :health
     resources :product_categories, only: %i[create index]
     resources :product_contents, only: %i[create]
-    resources :products, only: %i[create index show] do
+    resources :products, only: %i[create index show update] do
       resources :stocks, only: %i[index create]
     end
   end
