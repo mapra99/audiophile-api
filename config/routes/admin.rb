d380b1fd715dd51bd84097be127cff7c934d5b81
Miguel Prada <mapra99@gmail.com>
2022-12-23 14:50:37 -0500

    wip

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -2,7 +2,7 @@ namespace :admin do
   namespace :v1 do
     resource :health, only: [:show], controller: :health
     resources :product_categories, only: %i[create index]
-    resources :product_contents, only: %i[create]
+    resources :product_contents, only: %i[create update]
     resources :products, only: %i[create index show] do
       resources :stocks, only: %i[index create]
     end
