7fa3e94b88cdcd5c44c04b162243cf36cb47a475
Miguel Prada <mapra99@gmail.com>
2023-12-21 18:22:39 -0500

    fix typos in seeds

--- db/seeds/featured_products.rb
+++ db/seeds/featured_products.rb
@@ -58,14 +58,14 @@ module Seeds
           params: {
             product_id: product.id,
             key: 'features',
-            content: 'As the headphones all others are measured against, the XX99 Mark II demonstrates over five \
+            content: "As the headphones all others are measured against, the XX99 Mark II demonstrates over five \
                       decades of audio expertise, redefining the critical listening experience. This pair of \
                       closed-back headphones are made of industrial, aerospace-grade materials to emphasize \
                       durability at a relatively light weight of 11 oz.\n\nFrom the handcrafted microfiber ear \
                       cushions to the robust metal headband with inner damping element, the components work \
                       together to deliver comfort and uncompromising sound. Its closed-back design delivers up to \
                       27 dB of passive noise cancellation, reducing resonance by reflecting sound to a dedicated \
-                      absorber. For connectivity, a specially tuned cable is included with a balanced gold connector.'
+                      absorber. For connectivity, a specially tuned cable is included with a balanced gold connector."
           }
         ).call
 
